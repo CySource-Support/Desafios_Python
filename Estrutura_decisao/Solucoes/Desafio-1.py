493347f76ab45b9712a53fef0659f2bfbc9bcbd7
Jonas <jonasluanas2@gmail.com>
2022-08-27 00:33:24 -0300

    first solution from challenges

---
+++ Estrutura_decisao/Solucoes/Desafio-1.py
@@ -0,0 +1,10 @@
+# Solution by Jonas
+
+num1 = int(input("Insert the first number: "))
+num2 = int(input("Insert the second number: "))
+if num1 > num2:
+  print(f"The biggest number is: First number {num1}")
+elif num2 > num1:
+  print(f"The biggest number is: Second number {num2}")
+else:
+  print("The number are the same!")
